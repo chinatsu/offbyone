5eb3bc3d85389c65451e4de41d6db266073c9cbd
chinatsu <lolexplode@gmail.com>
2017-10-20 14:20:44 +0200

    set up basic package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='offbyone',
+      version='0.1',
+      description='Produces off by one "errors" in prints and interactive mode',
+      url='https://github.com/chinatsu/offbyone',
+      author='chinatsu',
+      author_email='lolexplode@gmail.com',
+      license='MIT',
+      packages=['offbyone'],
+      zip_safe=False)
